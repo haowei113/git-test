b1ddb4f3023de746ee55aa3ff17c6e9883af07c6
haowei <asus@qq.com>
2024-05-17 15:42:37 +0800

    hot_fix commit

--- src/main/java/com/dhu/edu/GitTEST.java
+++ src/main/java/com/dhu/edu/GitTEST.java
@@ -4,5 +4,6 @@ public class GitTEST {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git2!");
+        System.out.println("hot-fix test!");
     }
 }
