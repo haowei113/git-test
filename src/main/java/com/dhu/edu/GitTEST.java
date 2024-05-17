1a2e0ee0deb566812c245e9440a54559360de1b5
haowei <asus@qq.com>
2024-05-17 15:27:08 +0800

    third commit

--- src/main/java/com/dhu/edu/GitTEST.java
+++ src/main/java/com/dhu/edu/GitTEST.java
@@ -3,6 +3,6 @@ package com.dhu.edu;
 public class GitTEST {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-
+        System.out.println("Hello Git2!");
     }
 }
