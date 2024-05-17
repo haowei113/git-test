b0423a96d9604aa4911268bef1390549cc045f85
haowei <asus@qq.com>
2024-05-17 15:25:11 +0800

    second commit

--- src/main/java/com/dhu/edu/GitTEST.java
+++ src/main/java/com/dhu/edu/GitTEST.java
@@ -3,12 +3,6 @@ package com.dhu.edu;
 public class GitTEST {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello Git2!");
-        System.out.println("Hello Git3!");
-        System.out.println("Hello Git4!");
-        System.out.println("master test!");
-        System.out.println("hot-fix test!");
-        System.out.println("push test!");
-        System.out.println("pull test!");
+
     }
 }
